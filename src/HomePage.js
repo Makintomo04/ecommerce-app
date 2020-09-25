48a33c926b660d9ebba41c99e1fda92141d3ebc0
Makintomo04 <1721809@alumni.brunel.ac.uk>
2020-09-25 23:55:59 +0100

    homepage component created + sass installed.

---
+++ src/HomePage.js
@@ -0,0 +1,48 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div className="home">
+      <div className="directory-menu">
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">MENS</h1>
+            <span className="subtitle">Shop Now</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">WOMEN</h1>
+            <span className="subtitle">Shop Now</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">SHOES</h1>
+            <span className="subtitle">Shop Now</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">ACCESSORIES</h1>
+            <span className="subtitle">Shop Now</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">ACTIVEWEAR</h1>
+            <span className="subtitle">Shop Now</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">BRANDS</h1>
+            <span className="subtitle">Shop Now</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
