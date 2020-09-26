3ad041df8dc2d6700fd35ae7ed1f92021a15d581
Makintomo04 <1721809@alumni.brunel.ac.uk>
2020-09-26 03:35:36 +0100

    Updates

---
+++ precache-manifest.0448817b4a7a244770ecd957ee329ea6.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eda68b347310c47a49f52d40ca703da1",
+    "url": "/Makintomo04/ecommerce-app/index.html"
+  },
+  {
+    "revision": "68eebdc36f62518b811a",
+    "url": "/Makintomo04/ecommerce-app/static/css/main.3b5e5def.chunk.css"
+  },
+  {
+    "revision": "2473450e986d3f006111",
+    "url": "/Makintomo04/ecommerce-app/static/js/2.6b034eae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Makintomo04/ecommerce-app/static/js/2.6b034eae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68eebdc36f62518b811a",
+    "url": "/Makintomo04/ecommerce-app/static/js/main.90a886e8.chunk.js"
+  },
+  {
+    "revision": "910ad144208972424c77",
+    "url": "/Makintomo04/ecommerce-app/static/js/runtime-main.f2a9ca87.js"
+  },
+  {
+    "revision": "fac7c75c83c1942c5466f0a1ab45907a",
+    "url": "/Makintomo04/ecommerce-app/static/media/accessory.fac7c75c.jpg"
+  },
+  {
+    "revision": "0a95648df9a7cd33de3c5ff8fe93ac32",
+    "url": "/Makintomo04/ecommerce-app/static/media/brand2.0a95648d.jpg"
+  },
+  {
+    "revision": "2af0247c6e8355415901c58961c8ce56",
+    "url": "/Makintomo04/ecommerce-app/static/media/creps.2af0247c.jpg"
+  },
+  {
+    "revision": "a9dea04a3b053d88d3b3b7ddd3ae344d",
+    "url": "/Makintomo04/ecommerce-app/static/media/men3.a9dea04a.jpg"
+  },
+  {
+    "revision": "52712c27babacdc1cf6cc5ea47ca86cc",
+    "url": "/Makintomo04/ecommerce-app/static/media/nike.52712c27.jpg"
+  },
+  {
+    "revision": "69141f75c072a7491d71090737d776af",
+    "url": "/Makintomo04/ecommerce-app/static/media/women2.69141f75.jpg"
+  }
+]);
