40a847821d53b4fe3d878c986de5c01a1d930b20
Makintomo04 <1721809@alumni.brunel.ac.uk>
2020-09-27 22:01:13 +0100

    imported 3D render animation for potential use as preloader.

--- src/pages/homepage/HomePage.js
+++ src/pages/homepage/HomePage.js
@@ -1,9 +1,13 @@
 import React from "react";
 import "./homepage.scss";
 import Directory from "../../components/directory/Directory";
+import render from "../../components/category/images/render.gif";
 const HomePage = () => {
   return (
     <div className="home">
+      {/* <div className="render">
+        <img src={render} />
+      </div> */}
       <Directory />
     </div>
   );
