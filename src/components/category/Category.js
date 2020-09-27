e988ce41cf2eeda0dd3727613efef661803f3c9b
Michael Akintomo <1721809@brunel.ac.uk>
2020-09-27 01:28:07 +0100

    restructuring category container + accomodating for changes in scss file.

--- src/components/category/Category.js
+++ src/components/category/Category.js
@@ -2,12 +2,13 @@ import React from "react";
 import "./Category.scss";
 
 const Category = ({ title, img_URL }) => (
-  <div
-    style={{
-      backgroundImage: `url(${img_URL})`,
-    }}
-    className="menu-item"
-  >
+  <div className="menu-item">
+    <div
+      className="background-image"
+      style={{
+        backgroundImage: `url(${img_URL})`,
+      }}
+    ></div>
     <div className="content">
       <h1 className="title">{title}</h1>
       <span className="subtitle">Shop Now</span>
