c4d93bdcc03df7ec81f95bbd11e2a3b39e7e9747
Michael Akintomo <1721809@brunel.ac.uk>
2020-09-27 01:57:09 +0100

    implemented hover states + implemented BEM naming conventions to Category component.

--- src/components/category/Category.js
+++ src/components/category/Category.js
@@ -2,16 +2,16 @@ import React from "react";
 import "./Category.scss";
 
 const Category = ({ title, img_URL }) => (
-  <div className="menu-item">
+  <div className="category">
     <div
-      className="background-image"
+      className="category__bg"
       style={{
         backgroundImage: `url(${img_URL})`,
       }}
     ></div>
-    <div className="content">
-      <h1 className="title">{title}</h1>
-      <span className="subtitle">Shop Now</span>
+    <div className="category__content">
+      <h1 className="category__title">{title}</h1>
+      <span className="category__subtitle">Shop Now</span>
     </div>
 
     {console.log(img_URL)}
