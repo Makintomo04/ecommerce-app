b133f0e81abb191b676c1d9d7203babadb6e9c81
Makintomo04 <1721809@alumni.brunel.ac.uk>
2020-10-12 18:55:14 +0100

    Fixed side effect of react router that prevents site from loading new page from top of the page.

--- src/index.js
+++ src/index.js
@@ -6,11 +6,14 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { store, persistor } from "./redux/store";
+import ScrollToTop from "./components/scrolltotop/ScrollToTop";
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
       <PersistGate persistor={persistor}>
-        <App />
+        <ScrollToTop>
+          <App />
+        </ScrollToTop>
       </PersistGate>
     </BrowserRouter>
   </Provider>,
