5f2e6fdf7295eaf1e09f3bc18f4820435c315a19
Michael Akintomo <1721809@brunel.ac.uk>
2020-10-27 21:40:52 +0000

    Gsap animation added to homepage banner.

--- src/pages/homepage/HomePage.js
+++ src/pages/homepage/HomePage.js
@@ -1,27 +1,46 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
+import { TweenMax, TimelineLite, Power3 } from "gsap";
 import "./homepage.scss";
-import banner from "../../components/category/images/bannerimg.jpg";
+import "../../App.scss";
+import bannerImage from "../../components/category/images/bannerimg.jpg";
 import Directory from "../../components/directory/Directory";
 import render from "../../components/category/images/render.gif";
 import Email_List from "../../components/email_list/Email_List";
 import Blog from "../../components/blog/Blog";
 import Button from "../../components/button/Button";
 const HomePage = () => {
+  let home = useRef(null);
+  let bannerwrapper = useRef(null);
+  let banner = useRef(null);
+  let bannerImg = useRef(null);
+  let tl = new TimelineLite();
+
+  useEffect(() => {
+    TweenMax.to(home, 0, { css: { visibility: "visible" } });
+    tl.from(banner, 1.2, { y: 1200, ease: Power3.easeOut }).from(
+      bannerImg,
+      5,
+      { scale: 1.5, ease: Power3.easeOut },
+      0.2
+    );
+  });
   return (
-    <div className="home">
+    <div ref={(el) => (home = el)} className="home">
       {/* <div className="render">
         <img src={render} />
       </div> */}
-      <div className="banner">
-        <div className="banner__text">
-          <h1 className="banner__text__heading">The Helmut Depay Archive</h1>
-          <p className="banner__text__subtext">
-            Collection currated by M1KE at MARQ-E London
-          </p>
-          <Button theme="hero">Shop Now</Button>
+      <div ref={(el) => (bannerwrapper = el)} className="banner-wrapper">
+        <div ref={(el) => (banner = el)} className="banner">
+          <div className="banner__text">
+            <h1 className="banner__text__heading">The Helmut Depay Archive</h1>
+            <p className="banner__text__subtext">
+              Collection currated by M1KE at MARQ-E London
+            </p>
+            <Button theme="hero">Shop Now</Button>
+          </div>
+          {/* <p className="banner__subtext">The Helmut Depay Archive</p> */}
+          <img ref={(el) => (bannerImg = el)} src={bannerImage} alt="" />
         </div>
-        {/* <p className="banner__subtext">The Helmut Depay Archive</p> */}
-        <img src={banner} alt="" />
       </div>
       <Directory />
       <div className="message-banner">
